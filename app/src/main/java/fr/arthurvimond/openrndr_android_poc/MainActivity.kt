3f81c0f1bb31936596a12f878adb2e44ae43a3f4
Arthur Vimond <arthurvimond@gmail.com>
2021-01-17 15:10:27 +0100

    feat(*): create base Android app

---
+++ app/src/main/java/fr/arthurvimond/openrndr_android_poc/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.arthurvimond.openrndr_android_poc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
