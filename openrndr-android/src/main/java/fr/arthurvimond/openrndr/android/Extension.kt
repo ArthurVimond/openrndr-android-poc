ed7134c594b2487e1fc4c294743dccea1ce08b37
Arthur Vimond <arthurvimond@gmail.com>
2021-01-17 15:53:25 +0100

    feat(openrndr-android): add Extension interface

---
+++ openrndr-android/src/main/java/fr/arthurvimond/openrndr/android/Extension.kt
@@ -0,0 +1,25 @@
+package fr.arthurvimond.openrndr.android
+
+/**
+ * Indicates the stage in which the extension is
+ */
+enum class ExtensionStage {
+    SETUP,
+    BEFORE_DRAW,
+    AFTER_DRAW
+}
+
+/**
+ * Defines a Program extension. This is the interface for developers of OPENRNDR extensions.
+ */
+interface Extension {
+    var enabled: Boolean
+    fun setup(program: Program) {}
+    fun beforeDraw(drawer: Drawer, program: Program) {}
+    fun afterDraw(drawer: Drawer, program: Program) {}
+
+    /**
+     * Shutdown is called when the host application is quit
+     */
+    fun shutdown(program: Program) {}
+}
