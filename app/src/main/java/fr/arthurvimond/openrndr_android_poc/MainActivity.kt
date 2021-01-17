55360c29288d29c49f18b53594f4326a99c7e7ab
Arthur Vimond <arthurvimond@gmail.com>
2021-01-17 16:00:23 +0100

    feat(app): run TemplateProgram in MainActivity

--- app/src/main/java/fr/arthurvimond/openrndr_android_poc/MainActivity.kt
+++ app/src/main/java/fr/arthurvimond/openrndr_android_poc/MainActivity.kt
@@ -2,10 +2,22 @@ package fr.arthurvimond.openrndr_android_poc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import fr.arthurvimond.openrndr.android.ui.ORFragment
+import fr.arthurvimond.openrndr_android_poc.utils.Utils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Utils.fullscreen(window)
+        Utils.wakeLock(window)
+
+        val fragment = ORFragment.newInstance()
+        val fragmentContainer = findViewById<FrameLayout>(R.id.fragmentContainer)
+
+        fragment.setView(fragmentContainer, this)
+        fragment.setApplication(templateApplication())
     }
 }
